6529eb353b5befc220ec96716152e0f20043c8a2
‘Belove <beloveolusola@gmail.com>
2023-02-16 17:26:51 -0800

    ’C-executable_file’

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-  char Mystr[] = "Programming is like building a multilingual puzzle.\n";
-
+  char Mystr[] = "Programming is like building a multilingual puzzle\n";
   puts(Mystr);
-
-  return 0;
+  return (0);
 
 }
