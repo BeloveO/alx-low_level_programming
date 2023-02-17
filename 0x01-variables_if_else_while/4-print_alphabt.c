8fbdf9295b02cf69e84d21a1bc3e4c7c7ced4a0a
‘Belove <beloveolusola@gmail.com>
2023-02-17 15:34:29 -0800

    ’Lowercase_alphabet_without_e_and_q’

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,11 @@ int main(void)
 {
 	char x;
 
-	for (x = 'a'; x <= 'z'; x++) && (x != 'e' || x != 'q')
-		putchar(x);
+	for (x = 'a'; x <= 'z'; x++) 
+	{
+		if (x != 'e' && x != 'q')
+			putchar(x);
+	}
 	putchar('\n');
 	return (0);
 }
