f7e717a8577d92e4fe25506c13fbd4befe95fd00
‘Belove <beloveolusola@gmail.com>
2023-02-16 18:43:39 -0800

    ’Standard_error’

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 2, msg, sizeof(msg));
+write(2, msg, sizeof(msg));
 return (1);
 }
