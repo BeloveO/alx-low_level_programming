6af594ef0234180fd97f8c31c3e0a647f0dd633d
‘Belove <beloveolusola@gmail.com>
2023-02-17 15:22:09 -0800

    ’Lowercase_and_uppercase_alphabet’

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in both lower and uppercase
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 “0-positive_or_negative.c- using if-else statements”
 “1-last_digit.c- get the last digit of a random number”
 “2-print_alphabet.c- Print the alphabet in lowercase”
+“3-print_alphabets.c- Print the alphabet in lowercase and uppercase”
