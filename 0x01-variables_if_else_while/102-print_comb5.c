36c5db3c325a94969cc00e39b435a6d35bc5dccf
‘Belove <beloveolusola@gmail.com>
2023-02-17 18:27:47 -0800

    ’Quadruple_digits’

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,33 +6,21 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0; b < 9; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			if (b <= a)
-			{
-				for (c = 0; c < 10; c++)
-				{
-					for (d = 1; d < 9; d++)
-					{
-						if (d >= c)
-						{
-							putchar((a % 10) + '0');
-							putchar((b % 10) + '0');
-							putchar(' ');
-							putchar((c % 10) + '0');
-							putchar((d % 10) + '0');
-						}
-						if (a == 9 && b == 8 && c == 9 && d == 9)
-							continue;
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+			putchar((a % 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b % 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
