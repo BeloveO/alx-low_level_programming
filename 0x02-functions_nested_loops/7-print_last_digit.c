581ecfd10cecccd94d39581d57e7be0efcc2151e
‘Belove <beloveolusola@gmail.com>
2023-02-21 12:30:13 -0800

    Last digit of integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,10 +4,13 @@
  *
  * @a: integer to be checked
  *
+ * @b: result of input modulo 10
  * Return: the last digit of the input
  */
 int print_last_digit(int a)
 {
+	int b;
+
 	b = a % 10;
 	return (b);
 }
