0eea9a2edaaa34aee372c4b6b22dfc54aad10205
‘Belove <beloveolusola@gmail.com>
2023-02-21 15:58:52 -0800

    Multiplication tables to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 		_putchar('0');
 		for (b = 1; b <= 9; b++)
 		{
-			int result = a * b;
+			result = a * b;
 			_putchar(',');
-                        _putchar(' ');
+			_putchar(' ');
 			if (result <= 9)
 			{
 				_putchar(' ');
