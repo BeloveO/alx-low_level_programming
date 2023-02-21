24647595a884db1880879ecb81b321c46072499d
‘Belove <beloveolusola@gmail.com>
2023-02-21 10:40:34 -0800

    Prints the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	while (a <= 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+		_putchar('\n')
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 “0-putchar.c- print _putchar using _putchar”
 1-alphabet.c- prints the alphabet in lowercase
+2-print_alphabet_x10.c- prints the alphabet in lowercase 10 times n
