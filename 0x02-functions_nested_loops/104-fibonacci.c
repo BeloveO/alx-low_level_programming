b19a04e2e76e6f278a6d9e94226add0bb7aa7367
‘Belove <beloveolusola@gmail.com>
2023-02-21 18:38:27 -0800

    First 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,40 @@ int main(void)
 {
 	int c;
 	long int n1 = 0, n2 = 1, sn;
+	long int n1_h1, n1_h2, n2_h1, n2_n2;
+	long int h1, h2;
 
-	for (c = 0; c <= 98; c++)
+	for (c = 0; c <= 92; c++)
 	{
 		sn = n1 + n2;
 		printf("%lu", sn);
 
 		n1 = n2;
 		n2 = sn;
+	}
+	n1_h1= n1 / 10000000000;
+	n1_h2= n1 % 10000000000;
+	n2_h1= n2 / 10000000000;
+	n2_h2= n2 % 10000000000;
 
+	for (c = 93; c < 99; c++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2+h2;
+		if (h2 > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%lu%lu", h1, h2);
 		if (c == 98)
 			printf("\n");
 		else
 			printf(", ");
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
 	}
 	return (0);
 }
