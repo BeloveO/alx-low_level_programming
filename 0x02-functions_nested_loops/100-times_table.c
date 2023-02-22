41d1a475298c0b0123cd24f36ff27c7f79505e08
‘Belove <beloveolusola@gmail.com>
2023-02-21 16:42:19 -0800

    Prints the n times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,10 +20,14 @@ void print_times_table(int n)
 				result = a * b;
 
 				if (result > 9)
+				{
 					_putchar(' ');
+					_putchar(result + '0');
 				else
+				{
 					_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
