0d88acfd82d8b7f758cad0e8718515ba5129e6c0
‘Belove <beloveolusola@gmail.com>
2023-02-23 07:15:46 -0800

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 integers
+ *
+ * @a: one integer
+ * @b: second integer
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	int mi = a * b;
+
+	return (mi);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,4 @@
 0-isupper.c - checks if a character is upper or lowercasen
 1-isdigit.c- checks if input is a digit
+2-mul.c- multiplies 2 integers
+2-mul.c- multiplies two integers
