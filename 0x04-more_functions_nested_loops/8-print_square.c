3b1f835e5297bfbfe11073ea25f80da9b1bd172e
‘Belove <beloveolusola@gmail.com>
2023-02-23 15:52:33 -0800

    Prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,17 +4,17 @@
  * print_diagonal - prints straight line n times.
  * @n: times diagonal line is printed.
  */
-void print_diagonal(int n)
+void print_square(int size);
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < i; j++)
 				_putchar(35);
 			_putchar('\n');
 		}
