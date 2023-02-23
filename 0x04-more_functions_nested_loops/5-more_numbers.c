8390641be6c73db8bddf70bc333ce825e82627a5
‘Belove <beloveolusola@gmail.com>
2023-02-23 08:13:11 -0800

    Prints to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,10 +6,11 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 15; b++)
+		for (b = 0; b <= 14; b++)
 			_putchar(b + '0');
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
