7b44b1f7c7f433684271fb517b43a3fa7a1970ec
‘Belove <beloveolusola@gmail.com>
2023-02-23 00:55:14 -0800

    Checks if positive_or_negative function works

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function to see if integer is positive or negative
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	positive_or_negative(c);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0-main.c - checks to test the positive or negative functionn
