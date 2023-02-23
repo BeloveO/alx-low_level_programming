216a92ff133129f3a5b0c737a43b1d47732fe87b
‘Belove <beloveolusola@gmail.com>
2023-02-23 08:10:18 -0800

    Prints to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * more_numbers - prints to 14 ten times
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+			_putchar(b + '0');
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 2-mul.c- multiplies two integers
 3-print_numbers.c- prints from 0-9
 4-print_most_numbers.c- prints to 9 except 2 and 4
+5-more_numbers.c- prints to 14 ten times
