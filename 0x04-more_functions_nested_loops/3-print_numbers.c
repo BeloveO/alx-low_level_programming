b576e0b4c2b3adea0fa590a3ebe147aec6b5187a
‘Belove <beloveolusola@gmail.com>
2023-02-23 07:38:16 -0800

    Prints digits to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,11 @@
  */
 void print_numbers(void)
 {
-	int a;
+	int a = 0;
 
-	while (a >= 48 && a <= 57)
+	while (a >= 0 && a <= 9)
 	{
-		_putchar(a);
+		_putchar(a + '0');
 		a++;
 	}
 	_putchar('\n');
