f70b1c970d58dd509a4040c5a6635e9016c89a15
‘Belove <beloveolusola@gmail.com>
2023-02-23 06:41:04 -0800

    Checks if character is uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * File: main.h
+ * Auth: Belove Olusola
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _isupper(int c);
+
+#endif
