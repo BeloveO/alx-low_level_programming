4290ea580ef2d1e543d3af20841ac89d24a84d24
‘Belove <beloveolusola@gmail.com>
2023-02-23 16:25:08 -0800

    Prints to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints from 1 to 100
+ *
+ * Return: always 0 (success)
  */
 int main(void)
 {
@@ -8,13 +10,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 = 0)
+		if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 = 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 3 = 0 && i % 5 = 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
+	return (0);
 }
