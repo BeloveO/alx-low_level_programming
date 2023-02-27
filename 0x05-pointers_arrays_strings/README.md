6958ed22eb0536bb41213646d64482367d01307f
‘Belove <beloveolusola@gmail.com>
2023-02-27 14:48:43 -0800

    Resets to 98

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c- takes integer and updates its pointer value to 98
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * File: main.h
+ * Auth: Belove Olusola
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+void reset_to_98(int *n);
+
+#endif
