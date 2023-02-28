f25f55774b78b6d1c34b166ff31e5e5d8fe7cef3
‘Belove <beloveolusola@gmail.com>
2023-02-27 17:35:03 -0800

    Print arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
+ * print_array - prints arrays
  *
+ * @a: array to be printed
+ * @n: number of elements in array
  */
 void print_array(int *a, int n)
 {
 	int b;
 
-	while (b < n)
+	for (b = 0; b < n; b++)
 	{
-		_putchar(a[b]);
-		_putchar(',');
-		_putchar(' ');
-		b++;
+		printf("%d", a[b]);
+		if (b != n - 1)
+			printf(", ");
 	}
+	printf("\n");
 }
