a3aa5ea68e4cc1a5dcc4c0dcad3abeec351a3089
‘Belove <beloveolusola@gmail.com>
2023-02-27 16:50:29 -0800

    Second half print

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,11 +7,14 @@
 void puts_half(char *str)
 {
 	int a = 0, b;
-
+	
 	while (*(str + a) != '\0')
 		a++;
 	if (a / 2 == 1)
+	{
 		b = (a - 1) / 2;
+		b += 1;
+	}
 	else 
 		b = a / 2;
 	for (; b < a; b++)
