b26cf6817fd588e8b51bdb3b9bc3ec057549fe2f
‘Belove <beloveolusola@gmail.com>
2023-03-02 15:43:54 -0800

    Reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,8 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j, k;
 
-	for (i = n - 1; i >= 0; i--)
-		a[i];
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		k = a[i];
+		a[i] = a[j];
+		a[j] = k;
+	}
 }
