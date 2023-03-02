44804264ae59860b68c42076586c7e499247fafd
‘Belove <beloveolusola@gmail.com>
2023-03-02 13:50:53 -0800

    Copies string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strncpy - function to copy the contents of one string to another
+ *
+ * @dest: string to be copied into
+ * @src: string to be copied
+ * @n: the number the string will be copied to
+ *
+ * Return: returns the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-strcat.c- concatenating two strings
 1-strncat.c- concatenates two strings of a particular length
+2-strncpy.c- copies the content of a string into another
