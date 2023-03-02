af793eaaabd07dbf8828fdfa178d73b7ba8e4bd0
‘Belove <beloveolusola@gmail.com>
2023-03-01 17:26:09 -0800

    Different concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,13 +17,14 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	while (n--)
 	{
-		while (src[j] != '\0')
+		while (src[i] != '\0')
 		{
 			dest[i] = src[j];
 			j++;
 			i++;
 		}
 	}
+	dest[i] = '\0';
 	
 	return (dest);
 }
