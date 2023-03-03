754b06600164519cbf48d8619893bd12c1b865bf
‘Belove <beloveolusola@gmail.com>
2023-03-02 19:14:13 -0800

    Print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,18 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int i;
+	unsigned int i = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-
-	i = n;
-	if (i >= 10)
+	if ((i / 10) > 0)
 	{
-		_putchar((i / 10) + '0');
+		print_number(i / 10);
 	}
 	_putchar((i % 10);
 }
