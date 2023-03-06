51cdfcc6b955028d36393590fb3475927084a9c5
‘Belove <beloveolusola@gmail.com>
2023-03-06 15:45:18 -0800

    Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - function to fill memory with constant byte
+ *
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes to be copied
+ *
+ * Return: returns the pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
