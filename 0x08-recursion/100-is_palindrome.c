6db87356309f9b5f0258d4ab2bb507c520ace7fc
‘Belove <beloveolusola@gmail.com>
2023-03-08 19:22:12 -0800

    Palindromes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,25 +8,27 @@ int _strlen(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen(s + 1));
+	s++;
+	return (_strlen(s + 1));
 }
 
 /**
  * _palindrome - checks if string is equal both ways
- * @i: array position for string
- * @j: reversed array position for string
  * @s: string checked
  * Return: 1 if string is a palindrome and 0 if not
  */
-int _palindrome(char *s, int i, int j)
+int _palindrome(char *s)
 {
-	if (*(s + i) == *(s + j))
+	int i = _strlen(s) - 1;
+
+	if (*s == s[i])
 	{
-		if (i == j || i == j + 1)
-			return (1);
-		return (0 + _palindrome(s, i + 1, j - 1));
+		s++;
+		i--;
 	}
-	return (0);
+	else
+		return (0);
+	return (1);
 }
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not
@@ -37,5 +39,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (_palindrome(s, 0, _strlen(s) + 1));
+	return (_palindrome(s));
 }
