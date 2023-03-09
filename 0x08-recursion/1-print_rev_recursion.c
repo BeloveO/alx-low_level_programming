50a95388fff84df5a524a49c773288c8c80e0784
‘Belove <beloveolusola@gmail.com>
2023-03-08 16:37:06 -0800

    Reversed string print

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string to be modified
+ */
+void _print_rev_recursion(char *s)
+{
+	while (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
