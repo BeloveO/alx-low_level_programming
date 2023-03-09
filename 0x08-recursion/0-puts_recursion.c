0387c7b7cc59f6aedf3d60e01a5061cf452c6acf
‘Belove <beloveolusola@gmail.com>
2023-03-08 16:16:37 -0800

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		_putchar('\n');
+	_putchar(s[i]);
+	_puts_recursion(s[i + 1]);
+}
