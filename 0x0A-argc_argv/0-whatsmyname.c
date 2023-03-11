c772755102ba984facdf676555e225b6de403df8
‘Belove <beloveolusola@gmail.com>
2023-03-10 16:43:18 -0800

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function to print name of program
+ * @argc: number of characters in command line
+ * @argv: array of string on the command line
+ * Return: always success (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * File: main.h
+ * Auth: Belove Olusola
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x0A-argc_argv.
+ */
+
+int _putchar(char c);
+
+#endif
