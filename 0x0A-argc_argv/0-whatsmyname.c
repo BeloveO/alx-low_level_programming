b9df281133a29fd5cbb23441b32603cf660f2373
‘Belove <beloveolusola@gmail.com>
2023-03-10 16:56:33 -0800

    Print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 /**
  * main - function to print name of program
- * @argc: number of characters in command line
  * @argv: array of string on the command line
  * Return: always success (0)
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
