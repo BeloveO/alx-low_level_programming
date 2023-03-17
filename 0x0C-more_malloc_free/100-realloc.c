de1c7825b87672a255d1ff89e5b3f155bd77023b
‘Belove <beloveolusola@gmail.com>
2023-03-16 19:40:55 -0700

    Assigning new memory blocks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,9 +18,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-
-	ptr = malloc(new_size);
+	
 	free(ptr);
+	ptr = malloc(new_size);
 
 	return (ptr);
 }
