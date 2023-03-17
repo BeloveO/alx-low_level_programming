25af20a68afad650fc1cdde4da1b79fda2de2251
‘Belove <beloveolusola@gmail.com>
2023-03-16 19:38:57 -0700

    Assigning new memory blocks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,31 +9,18 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *nptr;
-	unsigned int i;
-
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-	{
 		ptr = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
-
-		return (ptr);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	nptr = malloc(new_size);
-	if (nptr == NULL)
-		return (NULL);
-	for (i = 0; i < old_size && i < new_size, i++)
-		nptr[i] = ((char *) ptr)[i];
+	ptr = malloc(new_size);
 	free(ptr);
 
-	return (nptr);
+	return (ptr);
 }
