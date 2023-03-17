c4e99455367c55e85db47261d5d489500a131131
‘Belove <beloveolusola@gmail.com>
2023-03-16 18:29:20 -0700

    Array memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *a;
+	unsigned int i;
+	char *a;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -16,4 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = 0;
+	return (a);
 }
