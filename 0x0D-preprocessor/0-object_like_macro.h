cf9a31f41662017cf722640347fb679a6722802f
‘Belove <beloveolusola@gmail.com>
2023-03-20 15:22:22 -0700

    Macro header file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,7 +2,8 @@
 #define _OBJECT_LIKE_MACRO_H
 
 /**
- * File: main.h
+ * File: 0-object_like_macro.h
+ * SIZE - macro for the abbreviation of token 1024
  * Auth: Belove Olusola
  * Desc: Header file containing Macro named SIZE as an
  *       abbreviation for token 1024.
