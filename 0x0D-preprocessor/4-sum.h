cc34d933ce99d385cd4ce1ff6d772cbacabe6352
Belove Olusola <beloveolusola@gmail.com>
2023-03-21 00:37:22 +0100

    Macro summation

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+/**
+ * File: 4-sum.h
+ * Auth: Belove Olusola
+ * Desc: Header file with a function-like
+ *       macro that computes the sum of two numbers
+ * SUM: macro to compute sum of two numbers
+ * @x: number to be added
+ * @y: number to be added
+*/
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
