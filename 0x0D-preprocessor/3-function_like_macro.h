1b527ac3d9cc6ed126f3be241383398ff09983b4
Belove Olusola <beloveolusola@Belove.local>
2023-03-21 00:29:19 +0100

    Absolute number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,10 +2,11 @@
 #define _MACROF_H
 
 /**
- * ABS(x): computes the absolute value of a number x
  * File: 3-function_like_macro.h
- * Auth: Belove Olusola 
- * Desc: Header file containing a function-like macro ABS(x) 
+ * ABS(x): computes the absolute value of a number x
+ * x: absolute number is checked
+ * Auth: Belove Olusola
+ * Desc: Header file containing a function-like macro ABS(x)
  *       that computes the absolute value of a number x
 */
 #define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
