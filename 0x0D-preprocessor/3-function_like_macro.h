b049448ef1b7ae72f037a43bf75865e25171779d
Belove Olusola <beloveolusola@Belove.local>
2023-03-21 00:10:29 +0100

    Absol ute value with macro function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,6 +3,10 @@
 
 /**
  * ABS(x): computes the absolute value of a number x
+ * File: 3-function_like_macro.h
+ * Auth: Belove Olusola 
+ * Desc: Header file containing a function-like macro ABS(x) 
+ *       that computes the absolute value of a number x
 */
 #define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
