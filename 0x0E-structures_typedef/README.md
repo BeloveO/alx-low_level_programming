8240b486ff5706c3168bd3c3da840a6e8920ac26
Belove Olusola <beloveolusola@gmail.com>
2023-03-21 01:30:42 +0100

    Structures in c

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,14 @@
+# 0x0E. C - Structures, typedef
+
+## Learning Objectives
+- What are structures, when, why and how to use them
+- How to use typedef
+
+## Tasks
+
+### 0. Poppy
+> Define a new type struct dog with the following elements:
+- name, type = char *
+- age, type = float
+- owner, type = char *
+** Solution: ** [dog.h]()
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - stores some information about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the dog owner
+ * File: dog.h
+ * Auth: Belove Olusola
+ * Desc: Header file for 0x0E-structures_typedef.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
