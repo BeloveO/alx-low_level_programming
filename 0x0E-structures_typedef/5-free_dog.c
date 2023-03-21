1f975d262f62381a12913de1ff104fe182d84ab3
‘Belove <beloveolusola@gmail.com>
2023-03-20 19:29:23 -0700

    Free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
