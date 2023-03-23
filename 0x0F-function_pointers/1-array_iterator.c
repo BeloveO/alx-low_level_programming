fe36dc175cb4cc5d27491a7de765361acff62093
‘Belove <beloveolusola@gmail.com>
2023-03-22 17:16:05 -0700

    Print out each element of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes function as a parameter on each element of array
+ * @array: array given
+ * @size: size of array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
