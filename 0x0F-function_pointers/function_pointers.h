0abb91653cf957ebfdd80240a883156b9c52ad1d
‘Belove <beloveolusola@gmail.com>
2023-03-22 17:05:59 -0700

    Function to print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
-/**
+/*
  * File: function_pointers.h
  * Auth: Belove Olusola
- * Desc: Header file containing prototypes to the files
- *       contained in 0x0F-function_pointers.
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
  */
+
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 
