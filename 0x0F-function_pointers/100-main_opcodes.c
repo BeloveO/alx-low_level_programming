ae30ef07f1f1ad85a72c314445c305ed58b99950
‘Belove <beloveolusola@gmail.com>
2023-03-22 19:23:46 -0700

    Printing opcodes of the same program

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the opcodes of its own function
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	char *opc = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return(0);
+}
+
