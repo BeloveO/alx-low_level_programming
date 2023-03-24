572a928c0c7f2b42c677d115538f6df05156d8f9
‘Belove <beloveolusola@gmail.com>
2023-03-23 18:00:22 -0700

    Sum of arguments variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all arguments given
+ * @n: the arguments given
+ * Return: sum of arguments given
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, s = 0;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		s += va_arg(ap, n);
+
+	va_end(ap);
+
+	return (s);
+}
