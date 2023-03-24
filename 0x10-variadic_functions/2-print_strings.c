9dc77ab96538edfb9db33ba4b182cc0b51171949
‘Belove <beloveolusola@gmail.com>
2023-03-23 18:40:53 -0700

    Printing string using variadic

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,9 +18,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 
 		printf("%s", str);
-		if (separator != NULL && i != n - 1)
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
