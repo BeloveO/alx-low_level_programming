4be9bcaa20d2e489fac988781fe9454658d53171
Belove <beloveolusola@gmail.com>
2023-04-05 02:47:51 +0100

    Free singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head of a linked list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	head = NULL;
+}
