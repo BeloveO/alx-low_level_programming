241fc950d58825ce40c2f664c90604de9212086c
Belove <beloveolusola@gmail.com>
2023-04-10 02:53:33 +0100

    Gets number of bits to flip n to m

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - gets number of bits needed to flip to get from number n to m
+ * @n: initial number given to be flipped
+ * @m: number to be flipped to
+ * Return: number of bits needed to flip n to m
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbits = 0, xor;
+
+	xor = n ^ m;
+	while (xor)
+	{
+		if (xor & 1)
+			nbits++;
+		xor >>= 1;
+	}
+	return (nbits);
+	
+}
