36536ed6b8e82b4181f62f98deba2e4fff4cb211
Belove <beloveolusola@gmail.com>
2023-05-18 19:58:22 -0700

    Print elements of a linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - Prints all the elements of a dlistint_t list.
@@ -8,15 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t i;
 
-	while (h)
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
-		nodes++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (i);
 }
 
